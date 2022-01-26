9a5b71995f901cddd5a200e03ec28c19239c0040
Lucian Miller <lucian.miller185@gmail.com>
2022-01-26 09:16:44 -0800

    update names

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'program-name',
+      title: 'martian-weather',
       template: './src/index.html',
       inject: 'body'
     }),
