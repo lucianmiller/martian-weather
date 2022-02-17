aa095ca0c9375f5f6bb70fed67c5aebc3916469b
Lucian Miller <lucian.miller185@gmail.com>
2022-02-17 09:06:01 -0800

    Add weather then statment and change names of vars

--- src/index.js
+++ src/index.js
@@ -4,22 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Rover from './rover';
 import Astronomy from './astronomy';
+import Weather from './weather';
 
 $(document).ready(function() {
   //Rover promise
-  let promise1 = Rover.getRover();
-  promise1.then(function(response) {
+  let promiseRover = Rover.getRover();
+  promiseRover.then(function(response) {
     let body = JSON.parse(response);
     console.log("Rover Working!");
   }, function(error) {
-    $('#error').text(`There was an error processing your request: ${error}`)
+    $('#error').text(`There was an error processing your request: ${error}`);
   });
   //Astronomy promise
-  let promise2 = Astronomy.getAstronomy();
-  promise2.then(function(response) {
+  let promiseAstro = Astronomy.getAstronomy();
+  promiseAstro.then(function(response) {
     let body = JSON.parse(response);
     console.log("Astronomy Working!");
   }, function(error){
-    $('#error').text(`There was an error processing your request: ${error}`)
+    $('#error').text(`There was an error processing your request: ${error}`);
+  });
+  //Weather promise
+  let promiseWeather = Weather.getWeather();
+  promiseWeather.then(function(response) {
+    let body = JSON.parse(response);
+    console.log("Weather working!");
+  }, function(error){
+    $('#error').text(`There was an error processing your request: ${error}`);
   });
 });
