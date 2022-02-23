416538091034ff849397d45476d15289ea512cb8
Lucian Miller <lucian.miller185@gmail.com>
2022-02-23 10:38:09 -0800

    Add specific camera to api call

--- src/rover.js
+++ src/rover.js
@@ -2,7 +2,7 @@ export default class Rover {
   static getRover() {
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`
+      const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${process.env.API_KEY}`
       request.onload = function(){
         if (this.status === 200) {
           resolve(request.response);
